5338fb073d949792b10f42825a56ab7e253c6adf
Steve Creswick <screswick@gmail.com>
2018-02-24 15:28:55 -0500

    Add patch request support

--- src/server/index.js
+++ src/server/index.js
@@ -2,6 +2,8 @@ const Koa = require('koa');
 const bodyParser = require('koa-bodyparser');
 const fs = require('fs');
 const morgan = require('koa-morgan');
+const cors = require('koa-cors');
+const refresh = require('koa-refresh');
 const session = require('koa-session');
 const passport = require('koa-passport');
 
@@ -9,10 +11,28 @@ const indexRoutes = require('./routes/index');
 const rockRoutes = require('./routes/rocks');
 const authRoutes = require('./routes/auth');
 const store = require('./session');
-
-
 const app = new Koa();
+
 const PORT = process.env.PORT || 1337;
+
+// All of the Below Can Be Promises added before the request!!!!
+// TODO: Add
+// Requires User
+// Requires Token
+// Requires Policy
+
+// Response Time
+async function responseTime (ctx, next) {
+  console.log('Started tracking response time')
+  const started = Date.now()
+  await next()
+  // once all middleware below completes, this continues
+  const ellapsed = (Date.now() - started) + 'ms'
+  console.log('Response time is:', ellapsed)
+  ctx.set('X-ResponseTime', ellapsed)
+}
+
+app.use(responseTime);
 
 // logging
 // create a write stream (in append mode)
@@ -29,6 +49,9 @@ app.use(session({ store }, app));
 // body parser
 app.use(bodyParser());
 
+// CORS
+app.use( cors( { methods: ['GET', 'PATCH', 'POST'] } ) );
+
 // authentication
 require('./auth');
 app.use(passport.initialize());
@@ -38,6 +61,9 @@ app.use(passport.session());
 app.use(indexRoutes.routes());
 app.use(rockRoutes.routes());
 app.use(authRoutes.routes());
+
+// Do Not Allow for Undefined Methods
+// app.use(router.allowedMethods())
 
 // server
 const server = app.listen(PORT, () => {
--- src/server/routes/rocks.js
+++ src/server/routes/rocks.js
@@ -7,6 +7,7 @@ const BASE_URL = `/api/v1/rocks`;
 router.get(BASE_URL, async (ctx) => {
   try {
     const rocks = await queries.getAllRocks();
+
     ctx.body = {
       status: 'success',
       data: rocks
@@ -83,7 +84,34 @@ router.post(`${BASE_URL}`, async (ctx) => {
 
 router.put(`${BASE_URL}/:id`, async (ctx) => {
   try {
-    const rock = await queries.updateRock(ctx.params.id, ctx.request.body);
+    const rock = await queries.updateRock(ctx.params.id );
+
+    if (rock.length) {
+      ctx.status = 200;
+      ctx.body = {
+        status: 'success',
+        data: rock
+      };
+    } else {
+      ctx.status = 404;
+      ctx.body = {
+        status: 'error',
+        message: 'That rock does not exist.'
+      };
+    }
+  } catch (err) {
+    ctx.status = 400;
+    ctx.body = {
+      status: 'error',
+      message: err.message || 'Sorry, an error has occurred.'
+    };
+  }
+});
+
+router.patch(`${BASE_URL}/:id`, async (ctx) => {
+  try {
+    const rock = await queries.updateRock( ctx.params.id, ctx.request.body );
+
     if (rock.length) {
       ctx.status = 200;
       ctx.body = {
